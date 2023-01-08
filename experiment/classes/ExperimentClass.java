6813259821d7edef45325c039b46841ed4da56d4
Mohamed OUTIDRARINE <mohamed.outidrarine@kleegroup.com>
2023-01-08 17:05:08 +0100

    file creation

---
+++ experiment/classes/ExperimentClass.java
@@ -0,0 +1,17 @@
+package experiment.classes;
+
+public class ExperimentClass {
+
+    public String getExperimentString() {
+        return experimentString;
+    }
+
+    public void setExperimentString(String experimentString) {
+        this.experimentString = experimentString;
+    }
+
+    private String experimentString = "test String";
+
+
+
+}
